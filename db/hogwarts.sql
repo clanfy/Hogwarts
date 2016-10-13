fd2ced0bdb44ef007ad9cb8e1547ae183191b36a
Clacson <cookiellacson@gmail.com>
2016-10-13 15:43:49 +0100

    hogwarts.sql file created

---
+++ db/hogwarts.sql
@@ -0,0 +1,21 @@
+DROP TABLE OwnedWants IF EXISTS;
+DROP TABLE Wands IF EXISTS;
+DROP TABLE Wizards IF EXISTS; 
+
+CREATE TABLE wizards(
+  id serial2 PRIMARY KEY,
+  name VARCHAR(255)
+  DA BOOLEAN
+);
+
+CREATE TABLE wands (
+  id serial4 PRIMARY KEY,
+  name VARCHAR(255),
+  wood_type VARCHAR(255)
+);
+
+CREATE TABLE ownedwands (
+  id serial4 PRIMARY KEY,
+  wizard_id INT4 REFERENCES wizards(id) ON DELETE CASCADE,
+  pokemon_id INT4 REFERENCES pokemons(id) ON DELETE CASCADE
+);
